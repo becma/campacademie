a2500ed8d191c3f75d72fc6251e7ef4cf73bac40
Maude Roxanne Bélanger-Cloutier <mauderoxanne.belangercloutier@nurun.com>
2023-11-26 15:29:36 -0500

    added more failsafes to sacs

--- themes/ca_theme/templates/sacs.php
+++ themes/ca_theme/templates/sacs.php
@@ -8,43 +8,52 @@
     $lunch = get_field('lunch');
     $lunch_global = get_field('lunch', $sites_global);
 
-    $sac_titre = !empty($sac['titre']) ? $sac['titre'] : $sac_global['titre'];
-    $sac_contenu = !empty($sac['contenu']) ? $sac['contenu'] : $sac_global['contenu'];
+    if (!empty($sac) || !empty($sac_global)) {
+        $sac_titre = !empty($sac['titre']) ? $sac['titre'] : $sac_global['titre'];
+        $sac_contenu = !empty($sac['contenu']) ? $sac['contenu'] : $sac_global['contenu'];
+    }
 
-    $lunch_titre = !empty($lunch['titre']) ? $lunch['titre'] : $lunch_global['titre'];
-    $lunch_contenu = !empty($lunch['contenu']) ? $lunch['contenu'] : $lunch_global['contenu'];
+    if (!empty($lunch) || !empty($lunch_global)) {
+        $lunch_titre = !empty($lunch['titre']) ? $lunch['titre'] : $lunch_global['titre'];
+        $lunch_contenu = !empty($lunch['contenu']) ? $lunch['contenu'] : $lunch_global['contenu'];
+    }
 
     $allergie = !empty(get_field('allergies_memo')) ? get_field('allergies_memo') : get_field('allergies_memo', $sites_global);
 ?>
 
-<?php if (!empty($sac) || !empty($sac_global)) : ?>
+<?php if (!empty($sac) || !empty($sac_global) || !empty($lunch) || !empty($lunch_global) || !empty($allergie)) : ?>
     <section class="sacs <?php if ($sacs_section_grey === true) {echo "-grey-section";} ?>">
         <div class="sacs-content">
             <div class="sacs-content-dans">
-                <div class="sac-a-dos">
-                    <h3>
-                        <?php echo $sac_titre ; ?>
-                    </h3>
-                    <div>
-                        <?php echo $sac_contenu; ?>
+                <?php if (!empty($sac) || !empty($sac_global)) : ?>
+                    <div class="sac-a-dos">
+                        <h3>
+                            <?php if (!empty($sac_titre)) { echo $sac_titre;} ?>
+                        </h3>
+                        <div>
+                            <?php if (!empty($sac_contenu)) { echo $sac_contenu; } ?>
+                        </div>
                     </div>
-                </div>
-                <div class="boite-a-lunch">
-                    <h3>
-                        <?php echo $lunch_titre ; ?>
-                    </h3>
-                    <div>
-                        <?php echo $lunch_contenu; ?>
+                <?php endif; ?>
+                <?php if (!empty($lunch) || !empty($lunch_global)) : ?>
+                    <div class="boite-a-lunch">
+                        <h3>
+                            <?php if (!empty($lunch_titre)) { echo $lunch_titre ; } ?>
+                        </h3>
+                        <div>
+                            <?php if (!empty($lunch_contenu)) { echo $lunch_contenu; } ?>
+                        </div>
                     </div>
-                </div>
+                <?php endif; ?>
             </div>
-            <?php
+            <?php if (!empty($allergie)) {
                 get_template_part('/templates/avertissement',
                     null,
                     array(
                         'class' => 'sacs-content-allergies',
                         'content' => $allergie,
-                    ))
+                    ));
+                }
             ?>
         </div>
     </section>
